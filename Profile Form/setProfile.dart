3e011e607bcd58ff0462300b5f8d95e8d0f8628c
Akanksha Singh <akankshasingh121297@gmail.com>
2020-07-06 02:45:21 +0530

    Update setProfile.dart

--- Profile Form/setProfile.dart
+++ Profile Form/setProfile.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:med_report/bloc_login/bloc/authentication_bloc.dart';
+import 'package:med_report/login/name.dart';
 
 class SetProfile extends StatefulWidget {
   @override
@@ -137,26 +139,41 @@ class MySetProfile extends State<SetProfile> {
                   ],
                 ),
                 new Padding(padding: EdgeInsets.only(top: 20.0)),
-                new TextFormField(
-                  decoration: new InputDecoration(
-                    labelText: "Gender",
-                    fillColor: Colors.white,
-                    border: new OutlineInputBorder(
-                      borderRadius: new BorderRadius.circular(25.0),
-                      borderSide: new BorderSide(),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Container(
+                       height: 55,
+                      width: MediaQuery.of(context).size.width/2,
+                      decoration: BoxDecoration(
+                        border: Border.all(color:Colors.black45,width: 1),
+                        borderRadius: BorderRadius.circular(25.0),
+                      ),
+                      child: 
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                           DropDownButtonGender(),
+                        ],
+                      ),
                     ),
-                   ),
-                  validator: (val) {
-                    if (val.length == 0) {
-                      return "Gender number cannot be empty";
-                    } else {
-                      return null;
-                    }
-                  },
-                  keyboardType: TextInputType.number,
-                  style: new TextStyle(
-                    fontFamily: "Poppins",
-                  ),
+                    new Padding(padding: EdgeInsets.only(left: 10.0)),
+                     Container(
+                       height: 55,
+                      width: MediaQuery.of(context).size.width/3,
+                      decoration: BoxDecoration(
+                        border: Border.all(color:Colors.black45,width: 1),
+                        borderRadius: BorderRadius.circular(25.0),
+                      ),
+                      child:
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          DropDownButtonBlg(),
+                        ],
+                      ), 
+                    ),
+                  ]
                 ),
                 new Padding(padding: EdgeInsets.only(top: 20.0)),
                 Row(
@@ -212,28 +229,6 @@ class MySetProfile extends State<SetProfile> {
                   ],
                 ),
                 new Padding(padding: EdgeInsets.only(top: 20.0)),
-                new TextFormField(
-                  decoration: new InputDecoration(
-                    labelText: "Blood Group",
-                    fillColor: Colors.white,
-                    border: new OutlineInputBorder(
-                      borderRadius: new BorderRadius.circular(25.0),
-                      borderSide: new BorderSide(),
-                    ),
-                   ),
-                  validator: (val) {
-                    if (val.length == 0) {
-                      return "Blood Group number cannot be empty";
-                    } else {
-                      return null;
-                    }
-                  },
-                  keyboardType: TextInputType.text,
-                  style: new TextStyle(
-                    fontFamily: "Poppins",
-                  ),
-                ),
-                new Padding(padding: EdgeInsets.only(top: 20.0)),
                       new TextFormField(
                         decoration: new InputDecoration(
                           labelText: "Address",
@@ -273,6 +268,7 @@ class MySetProfile extends State<SetProfile> {
                         ),
                       ),
                 new Padding(padding: EdgeInsets.only(top: 20.0)),
+                
                 RaisedButton(
                 child: Text('Save'),
                 color: Colors.blue[100],
@@ -288,3 +284,73 @@ class MySetProfile extends State<SetProfile> {
             )));
   }
 }
+
+class DropDownButtonGender extends StatefulWidget {
+  DropDownButtonGender({Key key}) : super(key: key);
+
+  @override
+  _DropDownButtonGenderState createState() => _DropDownButtonGenderState();
+}
+
+class _DropDownButtonGenderState extends State<DropDownButtonGender> {
+  String dropdownValue = 'Male';
+
+  @override
+  Widget build(BuildContext context) {
+    return DropdownButton<String>(
+      value: dropdownValue,
+      icon: Icon(Icons.arrow_downward),
+      iconSize: 24,
+      elevation: 16,
+      onChanged: (String newValue) {
+        setState(() {
+          dropdownValue = newValue;
+        });
+      },
+      items: <String>['Male', 'Female', 'Others']
+          .map<DropdownMenuItem<String>>((String value) {
+        return DropdownMenuItem<String>(
+          value: value,
+          child: Text(value,
+          style: TextStyle(color:Colors.black54)),
+        );
+      }).toList(),
+    );
+  }
+}
+
+class DropDownButtonBlg extends StatefulWidget {
+  DropDownButtonBlg({Key key}) : super(key: key);
+
+  @override
+  _DropDownButtonBlgState createState() => _DropDownButtonBlgState();
+}
+
+class _DropDownButtonBlgState extends State<DropDownButtonBlg> {
+  String dropdownValue = 'A+';
+
+  @override
+  Widget build(BuildContext context) {
+    return DropdownButton<String>(
+      value: dropdownValue,
+      icon: Icon(Icons.arrow_downward),
+      iconSize: 24,
+      elevation: 16,
+      onChanged: (String newValue) {
+        setState(() {
+          dropdownValue = newValue;
+        });
+      },
+      items: <String>['A+','A-','B+','B-','AB+','AB-','O+','O-']
+          .map<DropdownMenuItem<String>>((String value) {
+        return DropdownMenuItem<String>(
+          value: value,
+          child: Text(value,
+          style: TextStyle(color:Colors.black54)
+          ),
+        );
+      }).toList(),
+    );
+  }
+}
+
